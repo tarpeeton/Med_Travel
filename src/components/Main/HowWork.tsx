241289272aa6b57174302179904ef446cf053b68
tarpeeton <146853258+tarpeeton@users.noreply.github.com>
2024-10-03 17:50:37 +0500

    Reviews

--- src/components/Main/HowWork.tsx
+++ src/components/Main/HowWork.tsx
@@ -1,61 +1,66 @@
 import Image from 'next/image'
 import { FC } from 'react'
-import { TbCurrencyDollar } from "react-icons/tb";
-import { PiAirplaneTiltLight } from "react-icons/pi";
+import { TbCurrencyDollar } from "react-icons/tb"
+import { PiAirplaneTiltLight } from "react-icons/pi"
 
 
 const HowWork: FC = () => {
 	return (
-		<div className=' mx-[16px] mt-[120px]'>
+		<div className=' mx-[16px] mt-[120px] 2xl:mx-[200px]'>
 			<div className='flex flex-col'>
 				<div>
 					<p className='text-[25px] font-bold text-titleDark mdl:text-[35px]  2xl:text-[40px] '>Как мы работаем</p>
 				</div>
-				<div className='flex flex-col mt-[20px]'>
-					<div className='rounded-[20px] border border-borderColor py-[35px] px-[25px]'>
-						<div className='rounded-full flex items-center bg-green20 w-[70px] h-[70px] justify-center'>
-							<Image src='https://ucarecdn.com/115af432-16d4-4ed1-97cd-ada3a1ec0c9b/-/preview/24x26/' width={30} height={40} quality={100} alt='logo' />
+				<div className='flex flex-col mdl:flex-row mdl:flex-wrap mdl:gap-[2%] 2xl:flex-nowrap 2xl:mt-[30px]'>
+					<div className='flex flex-col mt-[20px] mdl:w-[48%] 2xl:w-[50%] '>
+						<div className='rounded-[20px] border border-borderColor py-[35px] px-[25px] 2xl:h-[380px]'>
+							<div className='rounded-full flex items-center bg-green20 w-[70px] h-[70px] justify-center'>
+								<Image src='https://ucarecdn.com/115af432-16d4-4ed1-97cd-ada3a1ec0c9b/-/preview/24x26/' width={30} height={40} quality={100} alt='logo' />
+							</div>
+							<div className='flex flex-col gap-[4px] mt-[25px]'>
+								<p className='text-green100 text-[20px] font-semibold font-raleway mdl:text-[25px]'>
+									Собираем документы
+								</p>
+								<p className='text-titleDark font-medium text-[15px] font-raleway mdl:text-[17px]'>
+									На этом этапе мы собираем все необходимые документы для вашего тура: паспорта, визы иrмедицинские справки. Мы помогаем вам подготовить и оформить все бумаги, чтобы ваш отдых был комфортным
+								</p>
+							</div>
 						</div>
-						<div className='flex flex-col gap-[4px] mt-[25px]'>
-							<p className='text-green100 text-[20px] font-semibold font-raleway'>
-							Собираем документы
-							</p>
-							<p className='text-titleDark font-medium text-[15px] font-raleway'>
-							На этом этапе мы собираем все необходимые документы для вашего тура: паспорта, визы иrмедицинские справки. Мы помогаем вам подготовить и оформить все бумаги, чтобы ваш отдых был комфортным
-							</p>
+					</div>
+					<div className='flex flex-col mt-[20px] mdl:w-[48%] 2xl:w-[50%] '>
+						<div className='rounded-[20px] border border-borderColor py-[35px] px-[25px] 2xl:h-[380px]'>
+							<div className='rounded-full flex items-center bg-green20 w-[70px] h-[70px] justify-center'>
+								<TbCurrencyDollar className='text-green100' size={40} />
+							</div>
+							<div className='flex flex-col gap-[4px] mt-[25px]'>
+								<p className='text-green100 text-[20px] font-semibold font-raleway mdl:text-[25px]'>
+									Проводим оплату
+								</p>
+								<p className='text-titleDark font-medium text-[15px] font-raleway mdl:text-[17px]'>
+									После подтверждения всех деталей вашего тура мы организуем безопасную оплату. Вы получите все необходимые инструкции для завершения транзакции, и мы предоставим подтверждение оплаты и бронирования
+								</p>
+							</div>
+						</div>
+					</div>
+					<div className='flex flex-col mt-[20px] 2xl:w-[50%] '>
+						<div className='rounded-[20px] border border-borderColor py-[35px] px-[25px] 2xl:h-[380px]'>
+							<div className='rounded-full flex items-center bg-green20 w-[70px] h-[70px] justify-center'>
+								<PiAirplaneTiltLight className='text-green100' size={40} />
+							</div>
+							<div className='flex flex-col gap-[4px] mt-[25px]'>
+								<p className='text-green100 text-[20px] font-semibold font-raleway mdl:text-[25px]'>
+									Отправляем вас в путь
+								</p>
+								<p className='text-titleDark font-medium text-[15px] font-raleway mdl:text-[17px]'>
+									На этом этапе мы организуем все детали вашего путешествия: бронируем билеты, обеспечиваем трансферы и проверяем готовность всех документов, чтобы ваш отдых начался без забот
+								</p>
+							</div>
 						</div>
 					</div>
 				</div>
-				<div className='flex flex-col mt-[20px]'>
-					<div className='rounded-[20px] border border-borderColor py-[35px] px-[25px]'>
-						<div className='rounded-full flex items-center bg-green20 w-[70px] h-[70px] justify-center'>
-						<TbCurrencyDollar className='text-green100'  size={40}/>
-						</div>
-						<div className='flex flex-col gap-[4px] mt-[25px]'>
-							<p className='text-green100 text-[20px] font-semibold font-raleway'>
-							Проводим оплату
-							</p>
-							<p className='text-titleDark font-medium text-[15px] font-raleway'>
-							После подтверждения всех деталей вашего тура мы организуем безопасную оплату. Вы получите все необходимые инструкции для завершения транзакции, и мы предоставим подтверждение оплаты и бронирования
-							</p>
-						</div>
-					</div>
-				</div>
-				<div className='flex flex-col mt-[20px]'>
-					<div className='rounded-[20px] border border-borderColor py-[35px] px-[25px]'>
-						<div className='rounded-full flex items-center bg-green20 w-[70px] h-[70px] justify-center'>
-						<PiAirplaneTiltLight className='text-green100'  size={40}/>
-						</div>
-						<div className='flex flex-col gap-[4px] mt-[25px]'>
-							<p className='text-green100 text-[20px] font-semibold font-raleway'>
-							Отправляем вас в путь
-							</p>
-							<p className='text-titleDark font-medium text-[15px] font-raleway'>
-							На этом этапе мы организуем все детали вашего путешествия: бронируем билеты, обеспечиваем трансферы и проверяем готовность всех документов, чтобы ваш отдых начался без забот
-							</p>
-						</div>
-					</div>
-				</div>
+
+
+
 			</div>
 		</div>
 	)
--- src/components/Main/Reviews.tsx
+++ src/components/Main/Reviews.tsx
@@ -1,10 +1,134 @@
-import {FC} from 'react';
+"use client"
+import { FC, useRef, useState } from "react"
+import Slider from "react-slick"
+import "slick-carousel/slick/slick.css"
+import { GrLinkNext, GrLinkPrevious } from "react-icons/gr"
+import Image from 'next/image'
+import { MdNavigateNext } from "react-icons/md";
 
+interface ReviewProps {
+  author: string;
+  date: string;
+  text: string;
+  imageUrl: string;
+}
 
 const Reviews: FC = () => {
+	const sliderRef = useRef<Slider | null>(null)
+  
+  // This array represents the reviews. You can replace this with your data source.
+	const reviews: ReviewProps[] = [
+		{
+			author: 'Иванов Иван',
+			date: '19.08.2024',
+			text: 'Мы с семьей недавно вернулись из тура по Узбекистану, и это было незабываемое путешествие! Все было организовано на высшем уровне — от комфортного проживания до увлекательных экскурсий. Особенно понравилось, что наши пожелания учитывались на каждом этапе поездки. Благодарим за отличный сервис и душевное отношение!',
+			imageUrl: 'https://ucarecdn.com/0d421291-2b4e-4a6c-a5d8-6293e75da8e7/-/preview/200x200/',
+		},
+		{
+			author: 'Иванов Иван',
+			date: '19.08.2024',
+			text: 'Мы с семьей недавно вернулись из тура по Узбекистану, и это было незабываемое путешествие! Все было организовано на высшем уровне — от комфортного проживания до увлекательных экскурсий. Особенно понравилось, что наши пожелания учитывались на каждом этапе поездки. Благодарим за отличный сервис и душевное отношение!',
+			imageUrl: 'https://ucarecdn.com/0d421291-2b4e-4a6c-a5d8-6293e75da8e7/-/preview/200x200/',
+		},
+		{
+			author: 'Иванов Иван',
+			date: '19.08.2024',
+			text: 'Мы с семьей недавно вернулись из тура по Узбекистану, и это было незабываемое путешествие! Все было организовано на высшем уровне — от комфортного проживания до увлекательных экскурсий. Особенно понравилось, что наши пожелания учитывались на каждом этапе поездки. Благодарим за отличный сервис и душевное отношение!',
+			imageUrl: 'https://ucarecdn.com/0d421291-2b4e-4a6c-a5d8-6293e75da8e7/-/preview/200x200/',
+		},
+    // Add more reviews as needed
+	];
+
+	const settings = {
+		dots: false,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: false,
+		autoplaySpeed: 3000,
+		arrows: false,
+	}
+
+	const handlePrev = () => {
+		if (sliderRef.current) {
+			sliderRef.current.slickPrev()
+		}
+	}
+
+	const handleNext = () => {
+		if (sliderRef.current) {
+			sliderRef.current.slickNext()
+		}
+	}
+
+  // Function to handle the "Читать полностью" click and log the full text
+	const handleReadMore = (text: string) => {
+		console.log('Full review text:', text);
+	}
+
   return (
-	<div>
-		Revivews
+	<div className='mt-[120px] bg-green100'>
+		<div>
+			<div className='flex flex-col py-[40px] px-[20px]'>
+				<p className='text-[#FFFFFF] text-[25px] font-bold font-raleway mdl:text-[35px] 2xl:text-[40px]'>Отзывы клиентов</p>
+
+				<div>
+					<Slider {...settings} ref={sliderRef}>
+						{reviews.map((review, index) => (
+							<div key={index} className='bg-white rounded-[20px] p-[20px] mt-[20px]'>
+								<div className='flex flex-col'>
+									<div className='flex flex-row gap-[8px]'>
+										<Image src={review.imageUrl} width={50} height={50} alt='User logo' className='rounded-full'/>
+										<div className='flex flex-col'>
+											<p className='text-[16px] text-titleDark mdl:text-[20px] font-semibold'>{review.author}</p>
+											<p className='text-[14px] text-[#A7A7A7] font-medium mdl:text-[16px]'>{review.date}</p>
+										</div>
+									</div>
+									<div className='mt-[20px]'>
+										{/* Displaying truncated text if longer than 250 characters */}
+										<p className='text-titleDark font-raleway font-medium'>
+											{review.text.length > 350 ? review.text.slice(0, 350) + '...' : review.text}
+										</p>
+									</div>
+									{review.text.length > 390 && (
+										<div className='mt-[20px]'>
+											<button
+												onClick={() => handleReadMore(review.text)}
+												className='text-green100 flex flex-row items-center text-[18px] font-semibold'
+											>
+												Читать полностью
+												<MdNavigateNext size={25} />
+											</button>
+										</div>
+									)}
+								</div>
+							</div>
+						))}
+					</Slider>
+				</div>
+
+				<div className='flex justify-end items-center mt-[30px]'>
+					<div className="flex flex-row gap-[8px]">
+						<button
+							onClick={handlePrev}
+							className="flex border border-[#E8E8E8] w-[55px] h-[55px] rounded-full items-center justify-center  mdl:w-[70px] mdl:h-[70px] 2xl:w-[78px] bg-white 2xl:h-[78px]"
+							aria-label="Previous slide"
+						>
+							<GrLinkPrevious size={20} className="text-black" />
+						</button>
+
+						<button
+							onClick={handleNext}
+							className="flex border bg-white border-[#E8E8E8] w-[55px] h-[55px] rounded-full items-center justify-center mdl:w-[70px] mdl:h-[70px] 2xl:w-[78px] 2xl:h-[78px]"
+							aria-label="Next slide"
+						>
+							<GrLinkNext size={20} className="text-black" />
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
   );
 };
