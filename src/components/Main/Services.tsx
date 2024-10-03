b0c788788a3f9d46d7c91b27ed989d5d683be6b1
tarpeeton <146853258+tarpeeton@users.noreply.github.com>
2024-10-03 15:17:16 +0500

    Service Mobile

--- src/components/Main/Services.tsx
+++ src/components/Main/Services.tsx
@@ -1,13 +1,9 @@
 "use client"
-import { FC, useRef, useState } from "react"
+import { FC, useRef } from "react"
 import Slider from "react-slick"
 import "slick-carousel/slick/slick.css"
-import { GrLinkNext } from "react-icons/gr"
-import { GrLinkPrevious } from "react-icons/gr"
+import { GrLinkNext, GrLinkPrevious } from "react-icons/gr"
 import Image from 'next/image'
-
-
-
 
 const Services: FC = () => {
 	const sliderRef = useRef<Slider | null>(null)
@@ -42,44 +38,102 @@ const Services: FC = () => {
 
 				<Slider {...settings} ref={sliderRef}>
 					<div className='flex flex-col mt-[25px]'>
-						<Image  src='https://ucarecdn.com/fc24f313-aae2-4923-bf26-76021146aa8f/-/preview/710x644/' width={300} height={300} alt='dfsdf'  className='object-cover w-full'/>
+						<Image
+							src='https://ucarecdn.com/fc24f313-aae2-4923-bf26-76021146aa8f/-/preview/710x644/'
+							width={300}
+							height={300}
+							alt='Туры по Узбекистану'
+							className='object-cover w-full'
+							loading="lazy"
+						/>
 
 						<div className='flex flex-col mt-[25px]'>
 							<p className='text-[22px] font-semibold '>Туры по Узбекистану</p>
 
 							<p className='text-[15px] font-raleway font-medium text-[#242424] py-[20px] border-b border-[#E8E8E8]'>
-							Посетите исторические центры Самарканда, Бухары и Хивы, где каждое здание дышит многовековой историей
+								Посетите исторические центры Самарканда, Бухары и Хивы, где каждое здание дышит многовековой историей
 							</p>
 							<p className='text-[15px] font-raleway font-medium text-[#242424] py-[20px] border-b border-[#E8E8E8]'>
-							Насладитесь традиционной узбекской кухней и ощутите настоящее гостеприимство местных жителей
+								Насладитесь традиционной узбекской кухней и ощутите настоящее гостеприимство местных жителей
 							</p>
 							<p className='text-[15px] font-raleway font-medium text-[#242424] py-[20px] border-b border-[#E8E8E8]'>
-							Совместите путешествие с оздоровительными процедурами в лучших санаториях и медицинских центрах
+								Совместите путешествие с оздоровительными процедурами в лучших санаториях и медицинских центрах
 							</p>
 							<p className='text-[15px] font-raleway font-medium text-[#242424] py-[20px] border-b border-[#E8E8E8]'>
-							Мы обеспечиваем комфортные условия проживания, профессиональное сопровождение и безопасность на протяжении всего тура
+								Мы обеспечиваем комфортные условия проживания, профессиональное сопровождение и безопасность на протяжении всего тура
 							</p>
 						</div>
 						<div className="flex flex-row items-center justify-between mt-[30px]">
-                    <button className=" w-[60%] mdl:w-[40%] bg-[#1AB2A6] text-white p-[16px] 2xl:w-[20%] rounded-[10px] font-bold">
-                      Найти тур
-                    </button>
-                    <div className="flex flex-row gap-[8px]">
-                      <button
-                        onClick={handlePrev}
-                        className="flex border border-[#E8E8E8] w-[40px] h-[40px]  rounded-full mdl:flex items-center justify-center 2xl:w-[70px] 2xl:h-[70px]"
-                      >
-                        <GrLinkPrevious size={20} className="text-black" />
-                      </button>
+							<button className="w-[50%] mdl:w-[40%] bg-[#1AB2A6] text-white text-[16px] p-[16px] 2xl:w-[20%] rounded-[10px] font-bold font-raleway">
+								Найти тур
+							</button>
+							<div className="flex flex-row gap-[8px]">
+								<button
+									onClick={handlePrev}
+									className="flex border border-[#E8E8E8] w-[55px] h-[55px] rounded-full items-center justify-center 2xl:w-[70px] 2xl:h-[70px]"
+									aria-label="Previous slide"
+								>
+									<GrLinkPrevious size={20} className="text-black" />
+								</button>
 
-                      <button
-                        onClick={handleNext}
-                        className="flex mdl:flex border border-[#E8E8E8] w-[40px] h-[40px] 2xl:w-[70px] 2xl:h-[70px] rounded-full items-center justify-center"
-                      >
-                        <GrLinkNext size={20} className="text-black" />
-                      </button>
-                    </div>
-                  </div>
+								<button
+									onClick={handleNext}
+									className="flex border border-[#E8E8E8] w-[55px] h-[55px] rounded-full items-center justify-center 2xl:w-[70px] 2xl:h-[70px]"
+									aria-label="Next slide"
+								>
+									<GrLinkNext size={20} className="text-black" />
+								</button>
+							</div>
+						</div>
+					</div>
+					<div className='flex flex-col mt-[25px]'>
+						<Image
+							src='https://ucarecdn.com/fc24f313-aae2-4923-bf26-76021146aa8f/-/preview/710x644/'
+							width={300}
+							height={300}
+							alt='Туры по Узбекистану'
+							className='object-cover w-full'
+							loading="lazy"
+						/>
+
+						<div className='flex flex-col mt-[25px]'>
+							<p className='text-[22px] font-semibold '>Туры по Узбекистану</p>
+
+							<p className='text-[15px] font-raleway font-medium text-[#242424] py-[20px] border-b border-[#E8E8E8]'>
+								Посетите исторические центры Самарканда, Бухары и Хивы, где каждое здание дышит многовековой историей
+							</p>
+							<p className='text-[15px] font-raleway font-medium text-[#242424] py-[20px] border-b border-[#E8E8E8]'>
+								Насладитесь традиционной узбекской кухней и ощутите настоящее гостеприимство местных жителей
+							</p>
+							<p className='text-[15px] font-raleway font-medium text-[#242424] py-[20px] border-b border-[#E8E8E8]'>
+								Совместите путешествие с оздоровительными процедурами в лучших санаториях и медицинских центрах
+							</p>
+							<p className='text-[15px] font-raleway font-medium text-[#242424] py-[20px] border-b border-[#E8E8E8]'>
+								Мы обеспечиваем комфортные условия проживания, профессиональное сопровождение и безопасность на протяжении всего тура
+							</p>
+						</div>
+						<div className="flex flex-row items-center justify-between mt-[30px]">
+							<button className="w-[50%] mdl:w-[40%] bg-[#1AB2A6] text-white text-[16px] p-[16px] 2xl:w-[20%] rounded-[10px] font-bold font-raleway">
+								Найти тур
+							</button>
+							<div className="flex flex-row gap-[8px]">
+								<button
+									onClick={handlePrev}
+									className="flex border border-[#E8E8E8] w-[55px] h-[55px] rounded-full items-center justify-center 2xl:w-[70px] 2xl:h-[70px]"
+									aria-label="Previous slide"
+								>
+									<GrLinkPrevious size={20} className="text-black" />
+								</button>
+
+								<button
+									onClick={handleNext}
+									className="flex border border-[#E8E8E8] w-[55px] h-[55px] rounded-full items-center justify-center 2xl:w-[70px] 2xl:h-[70px]"
+									aria-label="Next slide"
+								>
+									<GrLinkNext size={20} className="text-black" />
+								</button>
+							</div>
+						</div>
 					</div>
 				</Slider>
 			</div>
