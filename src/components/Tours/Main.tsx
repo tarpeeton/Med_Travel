37fb602d7f361338faf24f4ee4bef505b4e9457e
tarpeeton <146853258+tarpeeton@users.noreply.github.com>
2024-10-25 15:47:32 +0500

    updated (25.10)

--- src/components/Tours/Main.tsx
+++ src/components/Tours/Main.tsx
@@ -12,7 +12,6 @@ import Contacts from '../Main/Contacts'
 import useLocale from '@/hooks/useLocale'
 import { AllTours, AllTypes } from '@/lib/api'
 import { Tour, ApiResponse } from '@/interface/Tour'
-import Spinner from '../Spinner'
 import { Filters } from '@/interface/ToursFilter'
 
 
@@ -25,11 +24,11 @@ interface ITypes {
 const MainTours: FC = () => {
     const locale = useLocale()
     const [data, setData] = useState<Tour[]>([])
-    const [loading, setLoading] = useState(true)
-    const [error, setError] = useState<string | null>(null)
     const [types, setTypes] = useState<ITypes[]>([])
     const [typeId, setTypeID] = useState(0)
     const [isRefresh, setIsRefresh] = useState(false)
+
+    
 
     const [filters, setFilters] = useState<Filters>({
         fromAddress: '',
@@ -46,7 +45,6 @@ const MainTours: FC = () => {
     useEffect(() => {
         const fetchTours = async () => {
             try {
-                setLoading(true)
                 const res: ApiResponse = await AllTours(locale, {
                     ...filters
                 })
@@ -55,10 +53,7 @@ const MainTours: FC = () => {
                 setTypes(typeRes.data)
             } catch (err) {
                 console.error('Error fetching tours:', err)
-                setError('Failed to fetch tours')
-            } finally {
-                setLoading(false)
-            }
+            } 
         }
 
         fetchTours()
@@ -74,7 +69,7 @@ const MainTours: FC = () => {
             />
             <div className='mx-[16px] mdl:mx-[20px] 2xl:mx-[200px] relative mt-[420px] mdl:mt-[370px] 2xl:mt-[180px] flex flex-col gap-[120px] mdl:gap-[180px]'>
                 
-                        <Available tours={data} types={types} setTypeID={setTypeID} />
+                        <Available tours={data}  types={types} setTypeID={setTypeID} />
                         <Gallery/>
                         <Map />
                         <HowWork />
