fa8c0e7afdf87e95e6b7bc5a19781194d64fc014
tarpeeton <146853258+tarpeeton@users.noreply.github.com>
2024-11-22 15:28:55 +0500

    #fff

--- src/components/Main/Banner.tsx
+++ src/components/Main/Banner.tsx
@@ -7,7 +7,7 @@ import Script from "next/script"; // Script komponentini import qilish
  
 
 const MainBanner: FC = () => {
-  const clientId = "145340d2c41ddb0af95a6a10d7be041cbc229a";
+  const clientId = "145832671cce98f22847de5fbecf6e9090dfc7";
 
   return (
     <div className="relative">
@@ -17,7 +17,7 @@ const MainBanner: FC = () => {
         src={`//tourvisor.ru/module/init.js?clientId=${clientId}`}
         strategy="afterInteractive"
       />
-   <div className="tv-image-slider tv-moduleid-9975728"></div>
+   <div className="tv-image-slider tv-moduleid-9975743"></div>
      
     </div>
   );
