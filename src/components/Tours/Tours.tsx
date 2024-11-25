665011804782ee49f203d788bd66b43d6061de64
tarpeeton <146853258+tarpeeton@users.noreply.github.com>
2024-11-25 18:37:56 +0500

    new accaunt

--- src/components/Tours/Tours.tsx
+++ src/components/Tours/Tours.tsx
@@ -8,7 +8,7 @@ const Tours: FC = () => {
     return (
         <div >
             <div className='mt-[16px] mdl:mt-[20px] 2xl:mt-[25px] flex flex-col gap-[20px] mdl:flex-wrap mdl:flex-row'>
-            <div class="tv-hot-tours tv-moduleid-9975846"></div>
+            <div className="tv-hot-tours tv-moduleid-9975846"></div>
                 <Script
                     src={`//tourvisor.ru/module/init.js?clientId=${clientId}`}
                     strategy="afterInteractive"
